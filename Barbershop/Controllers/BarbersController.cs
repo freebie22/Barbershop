97bda3bf58338a9a37976e00f7a6c78e62cde986
freebie22 <bojkov755@gmail.com>
2023-03-20 15:01:17 +0200

    Операції над барберами повністю готові

--- Barbershop/Controllers/BarbersController.cs
+++ Barbershop/Controllers/BarbersController.cs
@@ -78,5 +78,145 @@ namespace Barbershop.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            BarbersVM barberVM = new BarbersVM()
+            {
+                Barber = new Barbers(),
+                WorkPostionsSelectList = _db.WorkPositions.Select(i => new SelectListItem
+                {
+                    Text = i.PositionName,
+                    Value = i.Id.ToString()
+                }
+                ),
+                Specializations = _db.Specializations.ToList()
+            };
+
+            barberVM.Barber = _db.Barbers.Find(id);
+            if (barberVM.Barber == null)
+            {
+                return NotFound();
+            }
+
+            return View(barberVM);
+        }
+
+        //POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(BarbersVM barberVM, int id)
+        {
+            var files = HttpContext.Request.Form.Files;
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            var objFromDb = _db.Barbers.AsNoTracking().FirstOrDefault(u => u.Id == barberVM.Barber.Id);
+
+            if (files.Count > 0)
+            {
+                string upload = webRootPath + WC.BarberPath;
+                string fileName = Guid.NewGuid().ToString();
+                string extension = Path.GetExtension(files[0].FileName);
+
+                var oldFile = Path.Combine(upload, objFromDb.BarberImage);
+
+                if (System.IO.File.Exists(oldFile))
+                {
+                    System.IO.File.Delete(oldFile);
+                }
+
+                using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+
+                barberVM.Barber.BarberImage = fileName + extension;
+            }
+
+            else
+            {
+                barberVM.Barber.BarberImage = objFromDb.BarberImage;
+            }
+
+
+            if (barberVM.SpecializationIds != null)
+            {
+                var selectedSpecializations = _db.Specializations.Where(s => barberVM.SpecializationIds.Contains(s.Id));
+
+                if (barberVM.Barber.Specializations != null)
+                {
+                    barberVM.Barber.Specializations.Clear();
+                }
+                else
+                {
+                    barberVM.Barber.Specializations = new List<Specializations>();
+                }
+
+                foreach (var specialization in selectedSpecializations)
+                {
+                    barberVM.Barber.Specializations.Add(specialization);
+                }
+            }
+
+            _db.Barbers.Update(barberVM.Barber);
+
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+        //GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            BarbersVM barberVM = new BarbersVM()
+            {
+                Barber = new Barbers(),
+                WorkPostionsSelectList = _db.WorkPositions.Select(i => new SelectListItem
+                {
+                    Text = i.PositionName,
+                    Value = i.Id.ToString()
+                }
+                ),
+                Specializations = _db.Specializations.ToList()
+            };
+
+            barberVM.Barber = _db.Barbers.Find(id);
+            if (barberVM.Barber == null)
+            {
+                return NotFound();
+            }
+
+            return View(barberVM);
+        }
+
+        //POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Barbers.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            string upload = _webHostEnvironment.WebRootPath + WC.BarberPath;
+
+            var oldFile = Path.Combine(upload, obj.BarberImage);
+
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+
+            _db.Barbers.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
