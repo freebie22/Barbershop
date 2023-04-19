0cef02a889e9abf5bc70572cae9e41c599373fa4
freebie22 <bojkov755@gmail.com>
2023-04-19 22:40:51 +0300

    Підтвердження пошти при реєстрації працює, треба відкорегувати стиль (з'їдждає повідомлення та не відображається картинка)

--- Barbershop/Program.cs
+++ Barbershop/Program.cs
@@ -1,6 +1,8 @@
 using Barbershop.Data;
 using Barbershop.Models;
+using Barbershop.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI().AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession(Options =>
 {
---
+++ Barbershop/Utility/EmailSender.cs
@@ -0,0 +1,59 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+
+namespace Barbershop.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("63308da312553ed4a633075861e03f70", "618c9b4f3f53d238bb8b1f42b70e3020")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "barbershop.oasis@ukr.net"},
+        {"Name", "Barbershop Oasis"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "Barbershop Oasis"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, 
+         {
+       "HTMLPart",
+       body
+      }, 
+     }
+             });
+             await client.PostAsync(request);
+        }
+    }
+}
