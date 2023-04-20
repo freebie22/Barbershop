ce2592822c9b865dcb35fb9fa798dd16b64cc38b
freebie22 <bojkov755@gmail.com>
2023-04-20 21:38:53 +0300

    Виправлено баг з запам'ятовуванням користувача

--- Barbershop/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Barbershop/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -190,8 +190,195 @@ namespace Barbershop.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = userId, code = code },
                             protocol: Request.Scheme);
 
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        await _emailSender.SendEmailAsync(Input.Email, "Реєстрація на веб-сайті Барбершоп 'Оазис'!",
+                            @"<!DOCTYPE html>
+                            <html>
+                            <head>
+                                <title></title>
+                                <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+                                <style type='text/css'>
+	
+                                    @media screen {
+                                        @font-face {
+                                            font-family: 'Lato';
+                                            font-style: normal;
+                                            font-weight: 400;
+                                            src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
+                                        }
+
+                                        @font-face {
+                                            font-family: 'Lato';
+                                            font-style: normal;
+                                            font-weight: 700;
+                                            src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
+                                        }
+
+                                        @font-face {
+                                            font-family: 'Lato';
+                                            font-style: italic;
+                                            font-weight: 400;
+                                            src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
+                                        }
+
+                                        @font-face {
+                                            font-family: 'Lato';
+                                            font-style: italic;
+                                            font-weight: 700;
+                                            src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
+                                        }
+                                    }
+
+                                    /* CLIENT-SPECIFIC STYLES */
+                                    body,
+                                    table,
+                                    td,
+                                    a {
+                                        -webkit-text-size-adjust: 100%;
+                                        -ms-text-size-adjust: 100%;
+                                    }
+
+                                    table,
+                                    td {
+                                        mso-table-lspace: 0pt;
+                                        mso-table-rspace: 0pt;
+                                    }
+
+                                    img {
+                                        -ms-interpolation-mode: bicubic;
+                                    }
+
+                                    /* RESET STYLES */
+                                    img {
+                                        border: 0;
+                                        height: auto;
+                                        line-height: 100%;
+                                        outline: none;
+                                        text-decoration: none;
+                                    }
+
+                                    table {
+                                        border-collapse: collapse !important;
+                                    }
+
+                                    body {
+                                        height: 100% !important;
+                                        margin: 0 !important;
+                                        padding: 0 !important;
+                                        width: 100% !important;
+                                    }
+
+                                    /* iOS BLUE LINKS */
+                                    a[x-apple-data-detectors] {
+                                        color: inherit !important;
+                                        text-decoration: none !important;
+                                        font-size: inherit !important;
+                                        font-family: inherit !important;
+                                        font-weight: inherit !important;
+                                        line-height: inherit !important;
+                                    }
+
+                                    /* MOBILE STYLES */
+                                    @media screen and (max-width:600px) {
+                                        h1 {
+                                            font-size: 32px !important;
+                                            line-height: 32px !important;
+                                        }
+                                    }
+
+                                    /* ANDROID CENTER FIX */
+                                    div[style*='margin: 16px 0;'] {
+                                        margin: 0 !important;
+                                    }
+                                </style>
+                            </head>
+
+                            <body style='background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;'>
+                            <!-- HIDDEN PREHEADER TEXT -->
+                            <div style='display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Lato, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;'> Вітаємо з реєстрацією на сторінці нашого Барбершопу!. </div>
+                            <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+                                <!-- LOGO -->
+                                <tr>
+                                    <td bgcolor='#000000' align='center'>
+                                        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+                                            <tr>
+                                                <td align='center' valign='top' style='padding: 40px 10px 40px 10px;'> </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td bgcolor='#000000' align='center' style='padding: 0px 10px 0px 10px;'>
+                                        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+                                            <tr>
+                                                <td bgcolor='#ffffff' align='center' valign='top' style='padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;'>
+                                                    <h1 style='font-size: 24px; font-weight: 400; margin: 2;'>Вас вітає Барбершоп 'Оазис'!</h1> <img src=""https://i.ibb.co/tDWZZnD/Oasis.jpg"" width = '325' height = '220' alt=""Oasis"" border=""0"" />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td bgcolor='#ffffff' align='center' style='padding: 0px 30px 20px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'>
+                                                    <p style='margin: 0;'>Вами було вказано даний E-Mail при реєстрації облікового запису в нашому веб-сервісі.</p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'>
+                                        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+                                           <!-- <tr>
+                                                <td bgcolor='#ffffff' align='left' style='padding: 20px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'>
+                                                    <p style='margin: 0; text-align:center'>YOUR OPT : *****</p>
+                                                </td>
+                                            </tr>-->
+                                            <tr>
+                                                <td bgcolor='#ffffff' align='left'>
+                                                    <table width='100%' border='0' cellspacing='0' cellpadding='0'>
+                                                        <tr>
+                                                            <td bgcolor='#ffffff' align='center' style='padding: 20px 30px 30px 30px;'>
+                                                                <table border='0' cellspacing='0' cellpadding='0'>
+                                                                    <tr>
+                                                                        <td align='center' style='border-radius: 3px;' bgcolor='#000000'><a href='" + HtmlEncoder.Default.Encode(callbackUrl) + @"' target='_blank' style='font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #000000; display: inline-block;'>Підтвердити</a></td>
+                                                                    </tr>
+                                                                </table>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr> <!-- COPY -->
+                                            <tr>
+                                                <td bgcolor='#ffffff' align='center' style='padding: 0px 30px 20px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'>
+                                                    <p style='margin: 0;'>Якщо ви не вказували даний E-Mail при реєстрації, або вказали інший - не переходьте за цим посиланням.</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td bgcolor='#ffffff' align='left' style='padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'>
+                                                    <p style='margin: 0;'>Наші соціальні мережі:</p>
+                                                    <div>
+                                                        <a style='padding-right:10px' href='https://www.instagram.com/boykov_artem'><img src='https://cdn-icons-png.flaticon.com/512/2111/2111463.png' width='25'></a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td bgcolor='#f4f4f4' align='center' style='padding: 30px 10px 0px 10px;'>
+                                        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+                                            <tr>
+                                                <td bgcolor='#000000' align='center' style='padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #fff; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'>
+                                                    <h2 style='font-size: 20px; font-weight: 400; color: #fff; margin: 0;'>Наші контакти</h2>
+                                                    <p style='margin: 0;'><a href='tel:+380685034088' target='_blank' style='color: #fff;'>Тел: +380-68-503-40-88</a></p>
+                                                        <p style='margin: 0;'><a href='mailto:barbershop.oasis@ukr.net' target='_blank' style='color: #fff;'>Email: barbershop.oasis@ukr.net</a></p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                            </body>
+                            </html>");
 
                         // If account confirmation is required, we need to show the link if we don't have a real email sender
                         if (_userManager.Options.SignIn.RequireConfirmedAccount)
