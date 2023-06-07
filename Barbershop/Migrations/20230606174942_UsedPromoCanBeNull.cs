7eb54a7983ee3d655c40cdb7728b3ad2c2daea1e
freebie22 <bojkov755@gmail.com>
2023-06-07 16:14:05 +0300

    Зроблено головну сторінку, виправлено футер

---
+++ Barbershop/Migrations/20230606174942_UsedPromoCanBeNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barbershop.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsedPromoCanBeNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UsedPromo",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UsedPromo",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Barbershop/Models/Appointment.cs
+++ Barbershop/Models/Appointment.cs
@@ -25,7 +25,7 @@ namespace Barbershop.Models
         [Required, Range(1, 10000, ErrorMessage = "Оберіть хоча б одну послугу")]
         public decimal TotalPrice { get; set; }
         public decimal TotalPriceWithDiscount { get; set; }
-        public string UsedPromo { get; set; }
+        public string? UsedPromo { get; set; }
         [Required]
         public virtual ICollection<Services> Services { get; set; }
         [Required(ErrorMessage = "Вкажіть свій номер телефону")]
